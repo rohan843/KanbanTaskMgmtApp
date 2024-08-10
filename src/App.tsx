2f43443c0e28adbcfcffcafbe7fb6b116e235665
rohan843 <rohan.sharma2934@gmail.com>
2024-08-10 16:31:22 +0530

    fix: apply theme provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="text-red-500">Hello</div>;
-}
+import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider>
+      <div className="text-red-500">Hello</div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
