239f7d5022c679c70cfd088f743f00206f70226b
rohan843 <rohan.sharma2934@gmail.com>
2024-08-11 17:16:49 +0530

    fix: begin design of task display

---
+++ src/components/KanbanTask.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { taskId } from "../types";
+
+// type Props = {
+//   taskId: taskId;
+// };
+
+const KanbanTask: React.FC = () => {
+  return (
+    <div>
+      <h1>Task Name</h1>
+      <p>0 of 2 subtasks</p>
+    </div>
+  );
+};
+
+export default KanbanTask;
