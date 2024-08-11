c26c89e7773da05a901c5478801ad009552a269d
rohan843 <rohan.sharma2934@gmail.com>
2024-08-11 19:55:50 +0530

    fix: add basic display data props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import KanbanColumn from "./components/KanbanWorkArea/KanbanColumn";
+import useKanbanProvider from "./hooks/useKanbanProvider";
 
 const App: React.FC = () => {
-  return <KanbanColumn />;
+  const { activeBoard, boardsData } = useKanbanProvider();
+  return (
+    activeBoard && (
+      <KanbanColumn columnIndex={0} columnData={boardsData[activeBoard][0]} />
+    )
+  );
 };
 
 export default App;
