c162aee9aa4982a2158e398f91f03b092d2dc43b
rohan843 <rohan.sharma2934@gmail.com>
2024-08-16 19:42:57 +0530

    fix: include layout provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { KanbanProvider } from "./context/KanbanContext.tsx";
+import { LayoutProvider } from "./context/LayoutContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <KanbanProvider>
       <ThemeProvider>
-        <App />
+        <LayoutProvider>
+          <App />
+        </LayoutProvider>
       </ThemeProvider>
     </KanbanProvider>
   </StrictMode>
